b8b845972f866faae1cada3e29b005b048fb2f26
Salome Ranson <salomeranson@gmail.com>
2021-06-28 08:39:10 -0400

    initial commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,43 @@
+import './SeasonDisplay.css';
+import React from 'react';
+
+
+
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Brrr, it is cold!',
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season]                       // Destructuring the seasonConfig object and passing in the season to grab the correct object value
+
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`${iconName} icon massive icon-left`} />
+            <h1>{text}</h1>
+            <i className={`${iconName} icon massive icon-right`} />
+        </div>
+    );
+};   
+
+
+export default SeasonDisplay;
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+
+    state = { lat: null, errorMessage: ''}
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({ lat: position.coords.latitude }), (err) => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        } else if (!this.state.errorMessage && this.state.lat) {
+            return <div><SeasonDisplay lat={this.state.lat} /></div>
+        } else {
+            return <div><Spinner message={'Please accept location request...'}/></div>
+        }
+    }
+
+    render() {
+        return (
+            <div className="testClass toAdd toAll views">
+                {this.renderContent()};
+            </div>
+        )
+    }
+}
+
+
+
+ReactDom.render(<App />, document.querySelector('#root'));
